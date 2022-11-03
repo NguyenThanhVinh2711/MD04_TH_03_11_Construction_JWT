d5d3dddb2d84cbaec8e078a4788bab4abe31d8df
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-11-03 16:36:40 +0700

    TH3-token

---
+++ src/main/java/com/codegym/service/IRoleService.java
@@ -0,0 +1,9 @@
+package com.codegym.service;
+
+
+import com.codegym.model.Role;
+import com.codegym.service.IGeneralService;
+
+public interface IRoleService extends IGeneralService<Role> {
+    Role findByName(String name);
+}
